c3b8dd42ab7e771688a8ce66289fb6ce961bd887
Dmitry Andriushkov <andrinshkovdmitryschoolkir@gmail.com>
2022-10-23 18:19:14 +0300

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,14 @@
+Number1 = int(input("Number1: "))
+Number2 = int(input("Number2: "))
+sign = input("Sign (+, -, *, /, **): ")
+if sign == '+':
+    print("Result: ", Number1 + Number2)
+elif sign == '-':
+    print("Result: ", Number1 - Number2)
+elif sign == '*':
+    print("Result: ", Number1 * Number2)
+elif sign == '/':
+    print("Result: ", Number1 / Number2)
+elif sign == '**':
+    print("Result: ", Number1 ** Number2)
+exit()
